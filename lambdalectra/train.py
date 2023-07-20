b387ad85380434c1259b97f32b34687fb0d9feba
Andrew Parry <2393265p@student.gla.ac.uk>
2023-07-20 15:15:02 +0100

    allow creation of run files

--- lambdalectra/train.py
+++ lambdalectra/train.py
@@ -4,23 +4,25 @@ import ir_datasets as irds
 from transformers import ElectraForSequenceClassification, ElectraTokenizer
 from fire import Fire
 import pandas as pd
+import os
 from os.path import join
 import json
 from trec23.pipelines.baselines import load_batchretrieve
 from trec23 import CONFIG
 
-def main(dataset_dir : str,
-         output_dir : str,
+def main(output_dir : str,
          batch_size : int,
          num_items : int,
          lr : float, 
          num_warmup_steps : int, 
          num_training_steps : int):
     
+    os.makedirs(output_dir, exist_ok=True)
+    
     query_lookup = pd.read_csv(join(CONFIG['MSMARCOv1_TEXT_PATH'], 'queries.tsv'), sep='\t', header=None, names=['query_id', 'query'])
     doc_lookup = pd.read_csv(join(CONFIG['MSMARCOv1_TEXT_PATH'], 'docs.tsv'), sep='\t', header=None, names=['docno', 'text'])
 
-    train_pairs = pd.read_csv(dataset_dir, sep='\t', header=None, names=['query_id', 'doc_id_a, doc_id_b'])
+    train_pairs = pd.read_csv(CONFIG['MSMARCOv1_TEXT_PATH'], 'triples_small.tsv', sep='\t', header=None, names=['query_id', 'doc_id_a, doc_id_b'])
     train_pairs = train_pairs[['query_id', 'doc_id_a']].rename(columns={'doc_id_a' : 'docno'})
     train_dataset = PairDataset(train_pairs, query_lookup, doc_lookup)
     train_loader = Loader(train_dataset, batch_size)
