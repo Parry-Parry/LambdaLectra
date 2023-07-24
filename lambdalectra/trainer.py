f65ea12f872afa1cbf407745ae8e29ef5180502f
Andrew Parry <2393265p@student.gla.ac.uk>
2023-07-24 16:11:09 +0100

    ewkjfnekjf

--- lambdalectra/trainer.py
+++ lambdalectra/trainer.py
@@ -47,12 +47,11 @@ class LambdaTrainer:
         docs = []
         labels = []
 
-        print(len(ranking))
-
         for row in ranking.itertuples():
             queries.append(row.query)
             docs.append(row.text)
             labels.append(row.label)
+            print(row.label)
         
         return (queries, docs), torch.Tensor(np.concatenate(labels, axis=0))
     
@@ -86,8 +85,6 @@ class LambdaTrainer:
 
     def main_loop(self, batch):
         batch, labels = self.first_pass(batch)
-        print(len(batch[0]))
-        print(len(labels))
         inputs = self.tokenizer(batch[0], batch[1], padding=True, truncation=True, return_tensors='pt')
         ids = inputs['input_ids'].to(self.device)
         mask = inputs['attention_mask'].to(self.device)
